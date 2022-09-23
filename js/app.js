f501322266486685a6118b6ec3a16ec7ec6da25b
GrudgeBeleker <bulkinbeleker@gmail.com>
2022-09-23 13:41:10 +0300

    modified:   css/style.css 	new file:   img/header_image.psd 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-window.addEventListener('scroll', e =>{
-    document.body.style.cssText - '--scrollTop: $(this.scrollY)px'
-});
+window.addEventListener('scroll', e => {
+    
+    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+  });
+
+  window.onscroll = function() {
+    var scrolled = window.pageYOffset;
+    console.log( 'Позиция скрола: '+scrolled  );
+};
